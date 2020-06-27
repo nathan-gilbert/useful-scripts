42198b60c7501d9b4c33a3557fd0a845fe0ba372
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-06-27 13:48:00 -0600

    game renamer script

--- game-renamer.fish
+++ game-renamer.fish
@@ -1,5 +1,9 @@
 # Renames game files so that retropie can more easily find metadata for them
 
 function game_renamer
-
+    for game in (ls *#\ NES*)
+        set newGame (string replace " # NES" "" $game)
+        echo "Renaming $game to $newGame"
+        mv $game $newGame
+    end
 end
